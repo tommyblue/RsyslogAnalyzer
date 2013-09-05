f1de7b1414d4f70c8437e6e3c2e3d6a53c298bb1
Tommaso Visconti <tommaso.visconti@gmail.com>
2013-09-05 16:05:52 +0200

    flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def flash_alerts
+    res = ''
+    flash.keys.each do |k|
+      res +=
+        content_tag :div, class: "alert #{ flash_class(k) } fade in", data: { alert: 'alert' } do
+          link_to('×', '#', class: 'close', data: { dismiss: 'alert' }) + content_tag(:p, flash[k])
+        end
+    end
+    res
+  end
+
+  def flash_class(k)
+    case k
+      when :info then 'alert-info'
+      when :error, :alert then 'alert-error'
+      when :notice, :success then 'alert-success'
+      else ''
+    end
+  end
 end
